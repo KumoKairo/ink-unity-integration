3128b9260d59328d02bcf28c78895852f1b9f1b9
Kirill <cyrilltoboe@gmail.com>
2018-05-30 23:12:25 +0300

    Initial compiler code

---
+++ Assets/CompilerTest/Editor/TestCompile.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+using System.IO;
+using Ink;
+
+public class TestCompile {
+
+	[Test]
+	public void TestCompileSimplePasses()
+	{
+	    var filePath = @"C:\Users\Kumo\Documents\Projects\Unity\InkUnity\Assets\story.ink";
+        var outputPath = @"C:\Users\Kumo\Documents\Projects\Unity\InkUnity\Assets\story.json";
+	    var compiler = new Compiler(File.ReadAllText(filePath));
+	    var story = compiler.Compile();
+	    var json = story.ToJsonString();
+        File.WriteAllText(outputPath, json);
+	}
+}
